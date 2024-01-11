afae16cb850b73d212883da5d9442d48708a414b
Alex Okeyo <alexokeyo15@gmail.com>
2024-01-11 14:48:49 +0300

    Navigation

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
-<script setup>
-
-</script>
-
 <template>
 
-  <div id="nav">
-    <router-link to="/">Home</router-link> 
-    <router-link to="/brazil">Brazil</router-link> 
-    <router-link to="/hawaii">Hawaii</router-link> 
-    <router-link to="/jamaica">Jamaica</router-link> 
-    <router-link to="/panama">Panama</router-link> 
+  <TheNavigation/>
 
-  </div>
-    <router-view></router-view>
   <div class="container">
-
+    <router-view></router-view>
   </div>
 </template>
+
+<script>
+
+import TheNavigation from '@/components/TheNavigation.vue'
+
+export default{
+
+  components:{TheNavigation}
+
+}
+
+</script>
 
