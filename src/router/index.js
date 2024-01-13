8f673b5f48b6f5f7f47b7abc870b41cba0e00351
Alex Okeyo <alexokeyo15@gmail.com>
2024-01-13 12:52:47 +0300

    Dynamic Routes

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,8 @@ const routes = [
     {path:'/brazil', name: 'brazil', component: ()=> import('@/views/Brazil.vue')},
     {path:'/hawaii', name: 'hawaii', component: ()=> import('@/views/Hawaii.vue')},
     {path:'/jamaica', name: 'jamaica', component: ()=> import('@/views/Jamaica.vue')},
-    {path:'/panama', name: 'panama', component: ()=> import('@/views/Panama.vue')}
+    {path:'/panama', name: 'panama', component: ()=> import('@/views/Panama.vue')},
+    {path:'/destination/:id',component: ()=> import('@/views/DestinationShow.vue')}
 ]
 
 const router = createRouter({
