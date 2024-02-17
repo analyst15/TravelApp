5a6212c76f77c2b1741ff0ad76153a3e81bca5c0
Alex Okeyo <alexokeyo15@gmail.com>
2024-02-17 10:49:55 +0300

    Scroll Behaviour

--- src/router/index.js
+++ src/router/index.js
@@ -40,6 +40,11 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes,
+    scrollBehavior (to, from, savedPosition){
+        return savedPosition || new Promise((resolve) => {
+            setTimeout(()=> resolve({ top: 0, behaviour: 'smooth'}), 300)
+        })
+    }
     /*linkActiveClass: 'vue-school-active-link'*/
 })
 
